8d6f22f7a9eae1103c5419be5fb2fe66c84e9df8
Aditi <adititayal9@gmail.com>
2020-08-16 11:54:19 +0530

    change image

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -13,9 +13,9 @@ export default class Introduction extends Component {
                   <div className="row">
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner js-fullheight">
-                        <div className="desc">
-                          <h1>Hi! <br />I'm Aditi</h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://drive.google.com/file/d/1gJz4eqCrqFit5p2j8osJYozRmi9lQk6g/view" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
+                        <div style={{color:"white"}} className="desc">
+                          <h1 style={{color:"white"}}>Hi! <br />I'm Aditi</h1>
+                          <p><a style={{color:"white"}} className="btn btn-primary btn-learn" href="https://drive.google.com/file/d/1gJz4eqCrqFit5p2j8osJYozRmi9lQk6g/view" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
                         </div>
                       </div>
                     </div>
@@ -29,8 +29,8 @@ export default class Introduction extends Component {
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner">
                         <div className="desc">
-                          <h1>I love building<br /> THINGS !!</h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://github.com/dbarochiya" target="_blank" rel="noopener noreferrer">View Projects <i className="icon-briefcase3" /></a></p>
+                          <h1 style={{color:"white"}}>I love building<br /> THINGS !!</h1>
+                          <p><a style={{color:"white"}}className="btn btn-primary btn-learn" href="https://github.com/adititayal9" target="_blank" rel="noopener noreferrer">View Projects <i className="icon-briefcase3" /></a></p>
                         </div>
                       </div>
                     </div>
@@ -44,8 +44,8 @@ export default class Introduction extends Component {
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner">
                         <div className="desc">
-                          <h1>I often <br/>Write ... </h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://dhruvbarochiya.me/" target="_blank" rel="noopener noreferrer">View Blog <i className="icon-book" /></a></p>
+                          <h1 style={{color:"white"}}>I often <br/>Write ... </h1>
+                          <p><a style={{color:"white"}}className="btn btn-primary btn-learn" href="https://adititayal.com" target="_blank" rel="noopener noreferrer">View Blog <i className="icon-book" /></a></p>
                         </div>
                       </div>
                     </div>
