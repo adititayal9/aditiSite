01232b35789c64bdfc7959a54c25e8a2510b9525
Aditi <adititayal9@gmail.com>
2020-08-16 14:43:01 +0530

    my first blog

--- src/components/blog.jsx
+++ src/components/blog.jsx
@@ -17,13 +17,13 @@ export default class Blog extends Component {
 					<div className="blog-entry">
 						<a href="blog.html" className="blog-img"><img src="images/blog-1.jpg" className="img-responsive" alt="HTML5 Bootstrap Template by colorlib.com" /></a>
 						<div className="desc">
-							<span><small>April 14, 2018 </small> | <small> Web Design </small> | <small> <i className="icon-bubble3" /> 4</small></span>
-							<h3><a href="blog.html">Renovating National Gallery</a></h3>
-							<p>Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+							<span><small>August 16, 2020 </small> | <small> Web Design </small> | <small> <i className="icon-bubble3" /> 4</small></span>
+							<h3><a href="https://docs.google.com/document/d/1Yu6KdUHvDdNgWPnRsm9M5tRm1FqHxRZJD_A05cW7DFY/view">Setting up my first website</a></h3>
+							<p>It is rightly said that it’s “Better late than never”.</p>
 						</div>
 					</div>
 					</div>
-					<div className="col-md-4 col-sm-6 animate-box" data-animate-effect="fadeInRight">
+					{/* <div className="col-md-4 col-sm-6 animate-box" data-animate-effect="fadeInRight">
 					<div className="blog-entry">
 						<a href="blog.html" className="blog-img"><img src="images/blog-2.jpg" className="img-responsive" alt="HTML5 Bootstrap Template by colorlib.com" /></a>
 						<div className="desc">
@@ -41,12 +41,12 @@ export default class Blog extends Component {
 							<h3><a href="blog.html">Make website from scratch</a></h3>
 							<p>Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
 						</div>
-					</div>
-					</div>
+					</div> */}
+					{/* </div> */}
 				</div>
 				<div className="row">
 					<div className="col-md-12 animate-box">
-					<p><a href="#" className="btn btn-primary btn-lg btn-load-more">Load more <i className="icon-reload" /></a></p>
+					<p><a href="blog.html" className="btn btn-primary btn-lg btn-load-more">Load more <i className="icon-reload" /></a></p>
 					</div>
 				</div>
 			</div>
